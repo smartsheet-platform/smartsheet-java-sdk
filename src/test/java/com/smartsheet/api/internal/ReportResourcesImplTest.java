123f9d7b9040a30113b28081f1b02e919d4b1d0a
Aditi Nioding <Aditi.Nioding@smartsheet.com>
2015-06-25 10:54:51 -0700

    Updated sendSheet test in ReportResourcesImplTest

--- src/test/java/com/smartsheet/api/internal/ReportResourcesImplTest.java
+++ src/test/java/com/smartsheet/api/internal/ReportResourcesImplTest.java
@@ -2,15 +2,18 @@ package com.smartsheet.api.internal;
 
 import com.smartsheet.api.SmartsheetException;
 import com.smartsheet.api.internal.http.DefaultHttpClient;
-import com.smartsheet.api.models.ObjectInclusion;
-import com.smartsheet.api.models.Report;
+import com.smartsheet.api.models.*;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.EnumSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by anioding on 6/24/15.
@@ -30,11 +33,21 @@ public class ReportResourcesImplTest extends ResourcesImplBase {
     public void testGetReport() throws SmartsheetException, IOException {
         server.setResponseBody(new File("src/test/resources/getReport.json"));
         Report report = reportResources.getReport(4583173393803140L, EnumSet.of(ObjectInclusion.ATTACHMENTS, ObjectInclusion.DISCUSSIONS), true, 1, 1);
-
+        assertEquals(report.getPermalink(), "https://app.smartsheet.com/b/home?lx=pWNSDH9itjBXxBzFmyf-5w");
+        assertTrue(report.getColumns().get(0).getVirtualId() == 4583173393803140L);
     }
 
     @Test
     public void testSendSheet() throws Exception {
+        server.setResponseBody(new File("src/test/resources/sendEmails.json"));
 
+        String[] emailAddress = { "someemail@somewhere.com" };
+        SheetEmail email = new SheetEmail();
+        email.setFormat(SheetEmailFormat.PDF);
+        FormatDetails format = new FormatDetails();
+        format.setPaperSize(PaperSize.A0);
+        email.setFormatDetails(format);
+        email.setTo(Arrays.asList(emailAddress));
+        reportResources.sendSheet(1234L, email);
     }
 }
