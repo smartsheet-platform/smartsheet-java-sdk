239d9288a1088874b49814f7bd6c84ae9c06ebba
Aditi Nioding <Aditi.Nioding@smartsheet.com>
2015-07-23 22:10:26 -0700

    Updated Smartsheet and SmartsheetImpl. Also updated license on AttachmentVersioningResource.

--- src/main/java/com/smartsheet/api/AttachmentVersioningResources.java
+++ src/main/java/com/smartsheet/api/AttachmentVersioningResources.java
@@ -1,5 +1,23 @@
 package com.smartsheet.api;
-
+/*
+ * #[license]
+ * Smartsheet SDK for Java
+ * %%
+ * Copyright (C) 2014 Smartsheet
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * %[license]
+ */
 import com.smartsheet.api.models.Attachment;
 import com.smartsheet.api.models.DataWrapper;
 import com.smartsheet.api.models.PaginationParameters;
@@ -10,7 +28,9 @@ import java.io.InputStream;
 import java.util.List;
 
 /**
- * Created by anioding on 7/17/15.
+ * <p>This interface provides methods to access AttachmentVersioning resources.</p>
+ *
+ * <p>Thread Safety: Implementation of this interface must be thread safe.</p>
  */
 public interface AttachmentVersioningResources {
 
