003e8bc7a4c5a50c60fa76b99b3fa9cc30c37138
Ron Reynolds <ron.reynolds@smartsheet.com>
2017-06-28 13:50:02 -0700

    removed unused (and mostly duplicate) generateCommaSeparatedListFavorites(); other code cleanup and simplification;

--- src/main/java/com/smartsheet/api/internal/util/QueryUtil.java
+++ src/main/java/com/smartsheet/api/internal/util/QueryUtil.java
@@ -22,9 +22,8 @@ package com.smartsheet.api.internal.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.List;
+import java.util.Collection;
 import java.util.Map;
-import java.util.Set;
 
 public class QueryUtil {
 
@@ -36,52 +35,22 @@ public class QueryUtil {
      * @param <T> the type
      * @return comma separated string
      */
-    public static <T> String generateCommaSeparatedList(Set<T> list) {
-
-        StringBuilder result = new StringBuilder();
-
-        if (list == null) {
-            return result.toString();
-        } else {
-            int index = 0;
-            for (Object obj : list) {
-                result.append(obj.toString());
-
-                if (index != list.size() - 1) {
-                    result.append(",");
-                }
-                index++;
-            }
-            return result.toString();
+    public static <T> String generateCommaSeparatedList(Collection<T> list) {
+        if (list == null || list.size() == 0) {
+            return "";
         }
-    }
-
-    public static <T> String generateCommaSeparatedListFavorites(List<T> list) {
-
         StringBuilder result = new StringBuilder();
-
-        if (list == null) {
-            return result.toString();
-        } else {
-            int index = 0;
-            for (Object obj : list) {
-                result.append(obj.toString());
-
-                if (index != list.size() - 1) {
-                    result.append(",");
-                }
-                index++;
-            }
-            return result.toString();
+        for (Object obj : list) {
+            result.append(',').append(obj.toString());
         }
+        return result.length() == 0 ? "" : result.substring(1);
     }
 
     public static String generateUrl(String baseUrl, Map<String, Object> parameters) {
         if (baseUrl == null) {
             baseUrl = "";
         }
-
-        return new StringBuilder().append(baseUrl).append(generateQueryString(parameters)).toString();
+        return baseUrl + generateQueryString(parameters);
     }
 
     /**
@@ -91,34 +60,22 @@ public class QueryUtil {
      * @return the query string
      */
     protected static String generateQueryString(Map<String, Object> parameters) {
-        StringBuilder result = new StringBuilder();
-
-        if (parameters == null) {
-            return result.toString();
-        } else {
-            try {
-                int parametersIndex = 0;
-                for(Map.Entry<String, Object> entry : parameters.entrySet()) {
-                    // Check to see if the key/value isn't null or empty string
-                    if (entry.getKey() != null && (entry.getValue() != null && !entry.getValue().toString().equals(""))) {
-                        if (parametersIndex == 0) {
-                            result.append("?");
-                        } else {
-                            result.append("&");
-                        }
-
-                        result.append(URLEncoder.encode(entry.getKey(), "utf-8"));
-                        result.append("=");
-                        result.append(URLEncoder.encode(entry.getValue().toString(), "utf-8"));
-
-                        parametersIndex++;
-                    }
-                }
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
-            }
-
-            return result.toString();
+        if (parameters == null || parameters.size() == 0) {
+            return "";
         }
+        StringBuilder result = new StringBuilder();
+        try {
+            for(Map.Entry<String, Object> entry : parameters.entrySet()) {
+                // Check to see if the key/value isn't null or empty string
+                if (entry.getKey() != null && (entry.getValue() != null && !entry.getValue().toString().equals(""))) {
+                    result.append('&').append(URLEncoder.encode(entry.getKey(), "utf-8")).append("=")
+                            .append(URLEncoder.encode(entry.getValue().toString(), "utf-8"));
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result.length() == 0 ? "" : "?" + result.substring(1);
     }
 }
