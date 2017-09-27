09bbe09763ec81018a5929934cdf51a0f850b7bc
Tim Wells <timwells@westtek.com>
2017-09-27 16:18:29 -0700

    API-1023
    Handle special case to serialize a null 'value' when linkInFromCell
    non-null.

---
+++ src/main/java/com/smartsheet/api/internal/json/CellSerializer.java
@@ -0,0 +1,63 @@
+package com.smartsheet.api.internal.json;
+/*
+ * #[license]
+ * Smartsheet SDK for Java
+ * %%
+ * Copyright (C) 2017 Smartsheet
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * %[license]
+ */
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.smartsheet.api.internal.util.Util;
+import com.smartsheet.api.models.Cell;
+import com.smartsheet.api.models.CellLink;
+
+/**
+ * Special case serializer to handle the linkInFromCell case. Normally we wouldn't serialize a null property,
+ * but in this case we must serialize a null for 'value'.
+ *
+ * We'll special case the linkInFromCell case but pass the rest along to the default serializer.
+ */
+public class CellSerializer extends JsonSerializer<Cell> {
+
+    private final JsonSerializer<Object> defaultSerializer;
+
+    public CellSerializer(JsonSerializer<Object> defaultSerializer) {
+        Util.throwIfNull(defaultSerializer);
+        this.defaultSerializer = defaultSerializer;
+    }
+
+    @Override
+    public void serialize(Cell cell, JsonGenerator gen, SerializerProvider serializers)
+            throws IOException  {
+
+        CellLink cell_link = cell.getLinkInFromCell();
+        if(cell_link == null) {
+            defaultSerializer.serialize(cell, gen, serializers);
+        }
+        else {
+            gen.writeStartObject();
+            gen.writeNumberField("columnId", cell.getColumnId());
+            gen.writeFieldName("linkInFromCell");
+            gen.writeObject(cell_link);
+            gen.writeNullField("value");
+            gen.writeEndObject();
+        }
+    }
+}
---
+++ src/main/java/com/smartsheet/api/internal/json/CellSerializerModifier.java
@@ -0,0 +1,40 @@
+package com.smartsheet.api.internal.json;
+/*
+ * #[license]
+ * Smartsheet SDK for Java
+ * %%
+ * Copyright (C) 2016 Smartsheet
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * %[license]
+ */
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import com.smartsheet.api.models.Cell;
+
+/**
+ * If the class being serialized is a cell, pass along the default serializer along to the CellSerializer object.
+ * We'll special case the linkInFromCell case but pass the rest along to the default serializer.
+ */
+public class CellSerializerModifier extends BeanSerializerModifier {
+    @Override
+    public JsonSerializer<?> modifySerializer(SerializationConfig config, BeanDescription beanDesc, JsonSerializer<?> serializer) {
+        if (beanDesc.getBeanClass() == Cell.class) {
+            return new CellSerializer((JsonSerializer<Object>) serializer);
+        }
+        return serializer;
+    }
+}
