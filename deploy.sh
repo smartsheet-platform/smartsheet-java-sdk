d4abfa6c88123fbc0aeb13dbd2e83db427a22769
Tim Wells <timwells@westtek.com>
2019-06-14 17:20:15 -0700

    !wip update travis config

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,28 @@ install_packages() {
 
 set -e
 export RELEASE_TAG=${deploy}
+
+echo $GPG_SECRET_KEYS | base64 --decode | gpg --import
+echo $GPG_OWNERTRUST | base64 --decode | gpg --import-ownertrust
+
 install_packages
 echo "beginning deploy..."
+
+# git configuration
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Automated Build"
+
+echo "updating release tag..."
+git tag ${RELEASE_TAG} -m "Release ${RELEASE_TAG}"
+git push https://${GH_USER}:${GH_ACCESS_TOKEN}@github.com/smartsheet-platform/smartsheet-python-sdk.git \
+    HEAD:${TRAVIS_BRANCH} --tags > /dev/null 2>&1
+
+mvn install --settings .maven.xml -DskipTests -Dgpg.skip -B
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${RELEASE_TAG}
+mvn clean deploy --settings .maven.xml -DskipTests -Prelease -B
+
+gitchangelog
+git add CHANGELOG.md
+git commit -am "chg: doc: update CHANGELOG.md"
+git push https://${GH_USER}:${GH_ACCESS_TOKEN}@github.com/smartsheet-platform/smartsheet-python-sdk.git \
+    HEAD:${TRAVIS_BRANCH} > /dev/null 2>&1
